ff725509379865b077403cf10e36b524a7771b06
José Simões <jose.simoes@eclo.solutions>
2022-10-04 18:47:21 +0100

    UsbStream now accepts GUID with device interface class

--- UsbClient/UsbClient.cs
+++ UsbClient/UsbClient.cs
@@ -11,11 +11,16 @@ namespace System.Device.UsbClient
         /// <summary>
         /// Creates an USB Stream from a WinUSB device that will use the specified name as the device description.
         /// </summary>
+        /// <param name="classId"><see cref="Guid"/> for the device class that will be used by WinUSB device.</param>
         /// <param name="name">Name to be used as device description.</param>
         /// <returns>A new UsbStream that was created with the specified name.</returns>
-        public static UsbStream CreateUsbStream(string name)
+        public static UsbStream CreateUsbStream(
+            Guid classId,
+            string name)
         {
-            return new UsbStream(name);
+            return new UsbStream(
+                classId,
+                name);
         }
     }
 }
