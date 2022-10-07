79224fbd1b3cf0fd77ea2211d78847d250e1e55d
José Simões <jose.simoes@eclo.solutions>
2022-10-07 16:25:26 +0100

    Update API (#3)

---
+++ UsbClient/UsbDeviceEvent.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+
+using nanoFramework.Runtime.Events;
+
+namespace System.Device.UsbClient
+{
+    internal class UsbDeviceEvent : BaseEvent
+    {
+        private UsbEventType _eventType;
+        private ushort _eventData;
+        private ushort _interfaceIndex;
+
+        public UsbEventType EventType { get => _eventType; set => _eventType = value; }
+
+        public ushort EventData { get => _eventData; set => _eventData = value; }
+
+        public ushort InterfaceIndex { get => _interfaceIndex; set => _interfaceIndex = value; }
+    }
+}
